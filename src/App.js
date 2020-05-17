82b1811aad9c1c8c44bbebe01aa1dba96c414ea7
Luka Kukina <lukscbt@gmail.com>
2020-05-17 17:15:55 +0200

    Add routes on homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Homepage from "./Pages/Homepage/Homepage"
+import { Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
