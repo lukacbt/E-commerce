2c2dba258e26095dc297bf4e9e1f7ae0c48db367
Luka Kukina <lukscbt@gmail.com>
2020-05-17 13:44:36 +0200

    Setup project and scss file for homepage

---
+++ src/Homepage.js
@@ -0,0 +1,43 @@
+import React from "react"
+import "./homepage.scss"
+
+const Homepage = () => {
+  return(
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">hats</h1>
+            <span className="subtitle">shop now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
